0406c9bf9ba0980683368a42129348fe29e920e7
BowHanna21 <amersarangani22@gmail.com>
2021-05-09 22:43:34 +0800

    added new setting display

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,14 +2,37 @@
   <q-layout view="hHh Lpr lff">
     <q-header v-if="this.$route.name == 'homeuser-page'" elevated>
       <q-toolbar>
-        <q-btn
-          icon="menu"
-          flat
-          size="20px"
-          @click="isDrawer = !isDrawer"
-        ></q-btn>
+        <q-btn icon="menu" flat size="20px" @click="isDrawer = !isDrawer"></q-btn>
         <q-toolbar-title>Guru App</q-toolbar-title>
-        <q-btn icon="settings" color="white" flat>
+
+        <q-btn-dropdown icon="settings" color="white" flat>
+          <div class="row no-wrap q-pa-md">
+            <div class="column">
+              <div class="text-h6 q-mb-md">Settings</div>
+              <q-toggle v-model="mobileData" label="Use Mobile Data" />
+              <q-toggle v-model="bluetooth" label="Bluetooth" />
+            </div>
+
+            <q-separator vertical inset class="q-mx-lg" />
+
+            <div class="column items-center">
+              <q-avatar size="72px">
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+
+              <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+
+              <q-btn label="logout"
+              class="full-width"
+              text-color="red"
+              to="/"
+              flat
+              @click="hideDrawer()" 
+              v-close-popup />
+            </div>
+          </div>
+        </q-btn-dropdown>
+        <!-- <q-btn icon="settings" color="white" flat>
           <q-menu padding fit :offset="[0, 15]">
             <q-btn
               label="logout"
@@ -21,7 +44,7 @@
               @click="hideDrawer()"
             ></q-btn>
           </q-menu>
-        </q-btn>
+        </q-btn> -->
       </q-toolbar>
     </q-header>
     <Drawer :isDrawer="isDrawer" @shoudHide="hideDrawer()" />
@@ -55,5 +78,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
